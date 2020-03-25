3569505634293ab66ca5e6af8783d65c60f2b4e6
Reynier-CES <rramirez@ces.com.uy>
2020-03-25 16:09:21 -0300

    Primera version

---
+++ Pages/Page_Login.py
@@ -0,0 +1,49 @@
+from telnetlib import EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+class LoginPage:
+    def __init__(self, driver):
+        self.driver = driver
+
+        self.user_textbox_id = "email"
+        self.password_id = "password"
+        self.login_button_id = "/html/body/div[1]/div[2]/form/div[3]/div[2]/button"
+
+
+    #   self.lgo.ico = "/html/body/div[1]/div[1]/img"
+    #   self.login_box_msg = "login-box-msg"
+    #   self.icon_email = "glyphicon glyphicon-envelope form-control-feedback"
+    #   self.icon_pass = "glyphicon glyphicon-lock form-control-feedback"
+    #   self.checkbox = "/html/body/div[1]/div[2]/form/div[3]/div[1]/div/label/input"
+
+    # def lgo_icon(self):
+    # self.driver.find_element_by_class_name(self.lgo_icon)
+
+    # def login_msg(self):
+    #   self.driver.find_element_by_class_name(self.login_box_msg)
+
+    # def icon_email(self):
+    #   self.driver.find_element_by_class_name(self.icon_email)
+
+    def enter_username(self, username):
+        self.driver.find_element_by_id(self.user_textbox_id).send_keys(username)
+
+    def icon_pass(self):
+        self.driver.find_element_by_class(self.icon_pass)
+
+    def enter_password(self, password):
+        self.driver.find_element_by_id(self.password_id).send_keys(password)
+
+    def check_box(self):
+        self.driver.find_element_by_xpath(self.checkbox)
+
+    def click_login(self):
+        self.driver.find_element_by_xpath(self.login_button_id).click()
+
+    # Method's de Espera
+    def esperar_boton(self, time):
+        WebDriverWait(self.driver, time).until(EC.presence_of_element_located((By.XPATH, self.login_button_id)))
+
---
+++ TestLogin/Login.py
@@ -0,0 +1,44 @@
+import unittest
+import csv
+import time
+from lib2to3.pgen2 import driver
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.alert import Alert
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.expected_conditions import alert_is_present
+from selenium.webdriver.support.expected_conditions import presence_of_element_located
+from Pages.Page_Login import LoginPage
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class LoginTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome(executable_path=r'C:\chromedriver_win32\chromedriver.exe')
+        cls.driver.maximize_window()
+        cls.driver.implicitly_wait(3)
+
+    def test_Login(self):
+        driver = self.driver
+        driver.get("http://localhost:8000/login")
+        time.sleep(3)
+
+        login = LoginPage(driver)
+        self.assertIn('Sistema de Gestión de Actividades Deportivas | Entrada', 'Sistema de Gestión de Actividades '
+                                                                                'Deportivas | Entrada')
+        login.enter_username("admin@jd.com")
+        login.enter_password("admin2019")
+        login.esperar_boton(5)
+        login.click_login()
+
+    def TearDown(self):
+        self.driver.close()
+
+
+if __name__ == '__main__':
+    unittest.main()
